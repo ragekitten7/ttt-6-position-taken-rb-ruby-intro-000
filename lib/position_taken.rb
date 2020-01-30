bbe363917132583aaf607d0448dd52d42ee7fde2
Sam Bryant <bryantsj@gmail.com>
2020-01-30 00:57:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,6 @@
 # code your #position_taken? method here
 def position_taken?(board, index)
- if (board[index] == " ") ||
-   (board[index] == "")
-
-    position_taken? = false
+ if (board[index] == " ") || (board[index] == "")
+position_taken? = false
 end
 end
